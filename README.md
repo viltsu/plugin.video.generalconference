869575c0518c69e6ba6524c85b57dd6355a21e2d
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2012-02-18 11:03:46 +0200

    Optimazing and code overhaul

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 LDS General Conference plugin for XBMC
 =============
 
+With this pluging you can watch LDS [general conferences](http://www.lds.org/general-conference) which have been released on [lds.org](http://www.lds.org) website using [XBMC](http://xbmc.org/) program. 
+
+This is **not** an official plugin by [The Church of Jesus Christ of Latter-day Saints (Mormons)](http://lds.org).
+
 To install
 -------
-1. go to the XBMC addon directory (on linux ~/.xbmc/addons)
-2. pull the sources there
+1. Go to https://github.com/viltsu/plugin.video.generalconference/downloads
+2. Download newest package (currently plugin.video.generalconference-0.0.2.zip)
+3. Launch XBMC
+4. Follow [these instructions to install the plugin](http://wiki.xbmc.org/index.php?title=Add-ons#How_to_install_from_zip)
 
+Options
+-------
+There is few options you can choose using the XMBC's plugin option menu.
+
+*Video Quality* can be chosen from the three qualities available on the web site (360p, 720p and 1080p). Videos are streamed streight from the server so choose well based on you conection speed or you'll end up buffering from a loong time :)
+
+*Video Language* at the moment only finnish and english are supported, but if you'd like to get it on any other language available available on general conferences webpage let me know (add an issue) and I'll add it.
+
+*Show only talks* Songs are avalable only in english so this has effect only when that language is selected. When selected only talks are displayed in the menu.
+
+Developer
+-------
+I'm a father of two young boys and a memeber of [The Church of Jesus Christ of Latter-day Saints](http://lds.org). At the moment I'm a student in the University of Helsinki (major in math). 
+
+Licence
+-------
+[GPLv2](http://www.gnu.org/licenses/gpl-2.0.html)
--- default.py
+++ default.py
@@ -10,105 +10,93 @@ onlytalks = __settings__.getSetting('only_talks') == "true"
 home = __settings__.getAddonInfo('path')
 fanart = xbmc.translatePath( os.path.join( home, 'fanart.jpg' ) )
 
-print str(onlytalks)
+def browser(url):
+    headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'}
+    req = urllib2.Request(url,None,headers)
+    response = urllib2.urlopen(req)
+    link=response.read()
+    return BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
 
 def conferences():
-        headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'}
-        req = urllib2.Request('http://www.lds.org/general-conference/conferences?lang=eng&clang='+videol,None,headers)
-        response = urllib2.urlopen(req)
-        link=response.read()
-        soup = BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
-        items = items = soup.findAll('ul',attrs={'class':'archive-by-month-list'})[0]('li')
-        for i in items:
-            #addDir(i.img['alt'],i.a['href'],1,'http://www.lds.org' + i.img['src'], '') 
-            addDir(i.img['alt'],i.a['href'],1,'', '') 
-        
-def sessions(url,iconimage):
-        headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'}
-        req = urllib2.Request(url,None,headers)
-        response = urllib2.urlopen(req)
-        link=response.read()
-        soup = BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
-        items = soup.findAll('session')
-        for i in items:
-            try:
-                addDir(i.string.encode('utf-8'),url,2, '', i['value'])
-            except:
-                pass
-
-def speakers(url,  session):
-        headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'}
-        req = urllib2.Request(url,None,headers)
-        response = urllib2.urlopen(req)
-        link=response.read()
-        soup = BeautifulSoup(link, convertEntities=BeautifulSoup.HTML_ENTITIES)
-        items = soup.findAll('table',attrs={'id':session})[0]('tr')
+    soup = browser('http://www.lds.org/general-conference/conferences?lang=eng&clang='+videol)
+    items = soup.findAll('ul',attrs={'class':'archive-by-month-list'})[0]('li')
+    for i in items:
         try:
-            url = items[1]('a', attrs={'class':videoq})[0]['href']
-            addLink('Play all', url,  '','',  3, '')
+            addDir(i.img['alt'],i.a['href'],1,'', '')
         except:
             pass
-        count = 0
-        for i in items:
-            if count < 2:
-                count += 1
-                continue
-            if onlytalks and i('span')[0]['class'] == 'song':
-                continue
-            try:
-                name =  i('span')[0].a.string.encode('utf-8') + ' - ' +  i('span')[1].string.encode('utf-8')
-                url = i('a', attrs={'class':videoq})[0]['href']
-                addLink(name,  url,  name, '',   3,  '')
-            except:
-                pass
+        
+def sessions(url,iconimage):
+    soup = browser(url)
+    items = soup.findAll('session')
+    for i in items:
+        try:
+            addDir(i.string.encode('utf-8'),url,2, '', i['value'])
+        except:
+            pass
+
+def speakers(url,  session):
+    soup = browser(url)
+    items = soup.findAll('table',attrs={'id':session})[0]('tr')
+    try:
+        url = items[1]('a', attrs={'class':videoq})[0]['href']
+        addLink('Play all', url,  '','',  3, '')
+    except:
+        pass
+    count = 0
+    for i in items:
+        if count < 2:
+            count += 1
+            continue
+        if onlytalks and i('span')[0]['class'] == 'song':
+            continue
+        try:
+            name =  i('span')[0].a.string.encode('utf-8') + ' - ' +  i('span')[1].string.encode('utf-8')
+            url = i('a', attrs={'class':videoq})[0]['href']
+            addLink(name,  url,  name, '',   3,  '')
+        except:
+            pass
 
 def playVideo(url):
-        item = xbmcgui.ListItem(path=url)
-        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
-
+    item = xbmcgui.ListItem(path=url)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-            params=sys.argv[2]
-            cleanedparams=params.replace('?','')
-            if (params[len(params)-1]=='/'):
-                params=params[0:len(params)-2]
-            pairsofparams=cleanedparams.split('&')
-            param={}
-            for i in range(len(pairsofparams)):
-                splitparams={}
-                splitparams=pairsofparams[i].split('=')
-                if (len(splitparams))==2:
-                    param[splitparams[0]]=splitparams[1]
-        return param
-
+    param=[]
+    paramstring=sys.argv[2]
+    if len(paramstring)>=2:
+        params=sys.argv[2]
+        cleanedparams=params.replace('?','')
+        if (params[len(params)-1]=='/'):
+            params=params[0:len(params)-2]
+        pairsofparams=cleanedparams.split('&')
+        param={}
+        for i in range(len(pairsofparams)):
+            splitparams={}
+            splitparams=pairsofparams[i].split('=')
+            if (len(splitparams))==2:
+                param[splitparams[0]]=splitparams[1]
+    return param
 
 def addLink(name,url,description,date,mode,iconimage):
-        try:
-            description = description + "\n \n Published: " + date
-        except:
-            description = "Published: " + date
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name,"Plot":description } )
-        liz.setProperty( "Fanart_Image", fanart )
-        liz.setProperty('IsPlayable', 'true')
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
-        return ok
-
-
+    try:
+        description = description + "\n \n Published: " + date
+    except:
+        description = "Published: " + date
+    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)
+    liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    liz.setInfo( type="Video", infoLabels={ "Title": name,"Plot":description } )
+    liz.setProperty( "Fanart_Image", fanart )
+    liz.setProperty('IsPlayable', 'true')
+    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+        
 def addDir(name,url,mode,iconimage, session):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&session="+urllib.quote_plus(session)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        liz.setProperty( "Fanart_Image", fanart )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-        return ok
-
+    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&session="+urllib.quote_plus(session)
+    liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+    liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    liz.setProperty( "Fanart_Image", fanart )
+    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        
 
 params=get_params()
 url=None
@@ -141,19 +129,12 @@ print "URL: "+str(url)
 print "Name: "+str(name)
 
 if mode==None or url==None or len(url)<1:
-    print ""
     conferences()
-
 elif mode==1:
-    print ""
     sessions(url, iconimage)
-
 elif mode==2:
-    print ""
     speakers(url, session)
-
 elif mode==3:
-    print ""
     playVideo(url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
