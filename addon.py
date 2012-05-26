689fd64ea437e57e3528caa74ae7da251910560b
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2012-05-26 08:43:23 +0300

    Remove duplicated lines

--- addon.py
+++ addon.py
@@ -33,7 +33,6 @@ class conferenceAddon(object):
         self.last = [1995, 2011][int(addon.getSetting('video_language'))]
 
     def showConferences(self):
-        #self._addDirectory(self.localized(30002), {'live':1})
         html = self._downloadUrl(CONFERENCES_URL + self.videol)	        
         for m in re.finditer("<li><a href=\"http://www.lds.org/general-conference/sessions/([^\"]+)\">([^<]+)</a></li>", html):
             year = m.group(1)[0:4]
@@ -43,19 +42,13 @@ class conferenceAddon(object):
             title = year + ' ' + m.group(2)
             self._addDirectory(title, {'url' :confurl,  'cid':title})
         xbmcplugin.endOfDirectory(self.handle)
-    
-    def _search(self, needle, hay):
-        r = re.search(needle, hay)
-        if not r:
-            return None
-        return r.group(1)
 
     def showConference(self,  cid,  url):
         confurl = url
         cachekey = cid + self.videol + self.videoq
         store = self.cache.get(cachekey)
         html = self._downloadUrl(url).replace('\n', '')
-        store = []
+        store = {}
         for m in re.finditer("<table class=\"sessions\" id=\"([^\"]+)\">(.*?)</table>", html):
             sessionHtml = m.group(2)
             sessionTitle = self._search("<session value=\"[^\"]+\">([^<]+)</session>", sessionHtml) or \
@@ -72,58 +65,52 @@ class conferenceAddon(object):
             allUrl = self._search("<a href=\"([^\"]+)\" class=\""+self.videoq+"\"", part) or \
                      self._search("<a href=\"([^\"]+)\" class=\"(video-360p|video-mp4|video-wmv)\"", part)
             if len(speakers) > 0 or allUrl != None:
-                store.append((m.group(1), sessionTitle, speakers, allUrl)) #Session Id, Session title, speakers, Play all url
+                store[m.group(1)] = (sessionTitle, speakers, allUrl) #Session Id, Session title, speakers, Play all url
+                self._addDirectory(sessionTitle, {'cid':cid,  'sid':m.group(1)})
         self.cache.set(cachekey,  repr(store))
-        for session, title, talks,  playallurl in store:
-            self._addDirectory(title, {'cid':cid,  'sid':session})
         xbmcplugin.endOfDirectory(self.handle)
     
     def showSession(self,  cid,  sid):
         cachekey = cid + self.videol + self.videoq
         try:
             store = eval(self.cache.get(cachekey))
+            title, talks, playAllUrl = store[sid]
         except:
             dialog = xbmcgui.Dialog()
-            dialog.ok("Error", " If this is first time after installing the plugin \n pleace restart the xbmc.")
-        first = True
-        for session, title, talks, playallurl in store:
-            if sid !=  session:
-                continue
-            if first and playallurl:
-                self._addLink(self.localized(30001),  {'vid':playallurl})
-            for talk in talks:
-                topic,  talker,  videourl=talk
-                self._addLink(talker + " - " + topic,  {'vid': videourl})
-            first = False
+            dialog.ok("Error", " If this is first time after installing the plugin \n place restart the xbmc.")
+            return
+        self._addVideo(self.localized(30001),  {'vid':playAllUrl})
+        for topic,  talker,  videoUrl in talks:
+             self._addVideo(talker + " - " + topic,  {'vid': videoUrl})
         xbmcplugin.endOfDirectory(self.handle)
 
     def playVideo(self, vid):
         item = xbmcgui.ListItem(path = vid)
         xbmcplugin.setResolvedUrl(self.handle, True, item)
-    
-    def playLive(self):
-        dialog = xbmcgui.Dialog()
-        dialog.ok("Error", "This has not been yet implemeted")
         
-    def _addLink(self,  title, urlparams,  thumb = 'DefaultVideo.png'):
-        item = xbmcgui.ListItem(title, iconImage = self.icon,  thumbnailImage=thumb)
-        item.setProperty('Fanart_Image', self.fanart)
-        item.setProperty('IsPlayable', 'true')
-        item.setInfo(type='video', infoLabels={
-            'title': title
-            })
-        url = self.path + '?' + urllib.urlencode(urlparams)
-        xbmcplugin.addDirectoryItem(self.handle, url, item)
-    
-    def _addDirectory(self,  title, urlparams,  thumb = 'DefaultFolder.png'):
-        item = xbmcgui.ListItem(title, iconImage = self.icon,  thumbnailImage=thumb)
-        item.setProperty('Fanart_Image', self.fanart)
-        item.setInfo(type='video', infoLabels={
-            'title': title
-            })
-        url = self.path + '?' + urllib.urlencode(urlparams)
-        xbmcplugin.addDirectoryItem(self.handle, url, item, isFolder = True)
+    def _search(self, needle, hay):
+        r = re.search(needle, hay)
+        if not r:
+            return None
+        return r.group(1)
 
+    def _addVideo(self,  title, urlparams,  thumb = 'DefaultVideo.png'):
+        self._addLine(title,  urlparams,  thumb,  True)
+        
+    def _addDirectory(self,  title, urlparams,  thumb = 'DefaultFolder.png'):
+        self._addLine(title,  urlparams,  thumb,  False)
+        
+    def _addLine(self, title,  urlparams,  thumb,  isPlayable):
+        item = xbmcgui.ListItem(title, iconImage = self.icon,  thumbnailImage=thumb)
+        item.setProperty('Fanart_Image', self.fanart)
+        item.setInfo(type='video', infoLabels={'title': title })
+        url = self.path + '?' + urllib.urlencode(urlparams)
+        if isPlayable:
+            item.setProperty('IsPlayable', 'true')
+            xbmcplugin.addDirectoryItem(self.handle, url, item)
+        else:
+            xbmcplugin.addDirectoryItem(self.handle, url, item, isFolder = True)
+        
     def _downloadUrl(self, url):
         r = urllib2.Request(url.replace('&amp;', '&'))
         r.add_header('User-Agent', USER_AGENT)
@@ -145,7 +132,5 @@ if __name__ == '__main__':
         conference.showSession(urllib.unquote_plus(PARAMS['cid'][0]),urllib.unquote_plus(PARAMS['sid'][0]))
     elif PARAMS.has_key('url'):
         conference.showConference(urllib.unquote_plus(PARAMS['cid'][0]),urllib.unquote_plus(PARAMS['url'][0]))
-    elif PARAMS.has_key('live'):
-        conference.playLive()
     else:
         conference.showConferences()
